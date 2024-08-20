a81ec49292215ab20aefcb2bc52d2a97f111ffe2
0x1Shub <shubhamyeram13@gmail.com>
2024-08-20 15:32:21 +0530

    Routing Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
 
 const App = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="*" element={<Navigate to={"/auth"} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
