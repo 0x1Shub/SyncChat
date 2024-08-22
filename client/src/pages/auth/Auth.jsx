c916b7a2ddbd3403a2ed1c270870e6a1edc69b72
0x1Shub <shubhamyeram13@gmail.com>
2024-08-22 18:37:41 +0530

    Server Setup

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -15,114 +15,23 @@ const Auth = () => {
   const handleSignup = async () => {};
 
   return (
-    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div
-        className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw]
-      xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
-      >
-        <div className="flex flex-col gap-10 items-center justify-center">
-          <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center">
-              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
-              <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
-            </div>
-            <p className="font-medium text-center">
-              Fill in the details to get started with the best chat app
-            </p>
-          </div>
-          <div className="flex items-center justify-center w-full">
-            <Tabs className="w-3/4">
-              <TabsList className="bg-transparent rounded-none w-full">
-                <TabsTrigger
-                  value="login"
-                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
-                >
-                  Login
-                </TabsTrigger>
-                <TabsTrigger
-                  value="signup"
-                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
-                >
-                  Signup
-                </TabsTrigger>
-              </TabsList>
-              <TabsContent className="flex flex-col gap-5 mt-10" value="login">
-                <Input
-                  placeholder="Email"
-                  type="email"
-                  className="rounded-full p-6"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                  placeholder="Password"
-                  type="password"
-                  className="rounded-full p-6"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button className="rounded-full p-6" onClick={handleLogin}>
-                  Login
-                </Button>
-              </TabsContent>
-              <TabsContent className="flex flex-col gap-5 " value="signup">
-                <Input
-                  placeholder="Email"
-                  type="email"
-                  className="rounded-full p-6"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                  placeholder="Password"
-                  type="password"
-                  className="rounded-full p-6"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Input
-                  placeholder="Confirm Password"
-                  type="password"
-                  className="rounded-full p-6"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                <Button className="rounded-full p-6" onClick={handleSignup}>
-                  Signup
-                </Button>
-              </TabsContent>
-            </Tabs>
-          </div>
-        </div>
-        <div className="hidden xl:flex justify-center items-center">
-          <img src={Background} alt="background login" className="h-[700px]" />
-        </div>
-      </div>
-    </div>
-
-    // <div className="h-[100vh] w-[100vw] flex items-center justify-center overflow-hidden">
+    // <div className="h-[100vh] w-[100vw] flex items-center justify-center">
     //   <div
-    //     className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[90vw] md:w-[85vw] lg:w-[75vw]
-    // xl:w-[65vw] rounded-3xl grid xl:grid-cols-2 overflow-hidden"
+    //     className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw]
+    //   xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
     //   >
-    //     <div className="flex flex-col gap-8 items-center justify-center p-4 md:p-8">
-    //       <div className="flex flex-col items-center justify-center">
+    //     <div className="flex flex-col gap-10 items-center justify-center">
+    //       <div className="flex items-center justify-center flex-col">
     //         <div className="flex items-center justify-center">
-    //           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-    //             Welcome
-    //           </h1>
-    //           <img
-    //             src={Victory}
-    //             alt="Victory Emoji"
-    //             className="h-[50px] md:h-[75px] lg:h-[100px] ml-2"
-    //           />
+    //           <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+    //           <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
     //         </div>
-    //         <p className="font-medium text-center text-sm md:text-base lg:text-lg">
+    //         <p className="font-medium text-center">
     //           Fill in the details to get started with the best chat app
     //         </p>
     //       </div>
     //       <div className="flex items-center justify-center w-full">
-    //         <Tabs className="w-full md:w-3/4">
+    //         <Tabs className="w-3/4">
     //           <TabsList className="bg-transparent rounded-none w-full">
     //             <TabsTrigger
     //               value="login"
@@ -137,54 +46,48 @@ const Auth = () => {
     //               Signup
     //             </TabsTrigger>
     //           </TabsList>
-    //           <TabsContent className="flex flex-col gap-5 mt-6" value="login">
+    //           <TabsContent className="flex flex-col gap-5 mt-10" value="login">
     //             <Input
     //               placeholder="Email"
     //               type="email"
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
+    //               className="rounded-full p-6"
     //               value={email}
     //               onChange={(e) => setEmail(e.target.value)}
     //             />
     //             <Input
     //               placeholder="Password"
     //               type="password"
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
+    //               className="rounded-full p-6"
     //               value={password}
     //               onChange={(e) => setPassword(e.target.value)}
     //             />
-    //             <Button
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
-    //               onClick={handleLogin}
-    //             >
+    //             <Button className="rounded-full p-6" onClick={handleLogin}>
     //               Login
     //             </Button>
     //           </TabsContent>
-    //           <TabsContent className="flex flex-col gap-5 mt-6" value="signup">
+    //           <TabsContent className="flex flex-col gap-5 " value="signup">
     //             <Input
     //               placeholder="Email"
     //               type="email"
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
+    //               className="rounded-full p-6"
     //               value={email}
     //               onChange={(e) => setEmail(e.target.value)}
     //             />
     //             <Input
     //               placeholder="Password"
     //               type="password"
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
+    //               className="rounded-full p-6"
     //               value={password}
     //               onChange={(e) => setPassword(e.target.value)}
     //             />
     //             <Input
     //               placeholder="Confirm Password"
     //               type="password"
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
+    //               className="rounded-full p-6"
     //               value={confirmPassword}
     //               onChange={(e) => setConfirmPassword(e.target.value)}
     //             />
-    //             <Button
-    //               className="rounded-full p-3 md:p-4 lg:p-5 w-full"
-    //               onClick={handleSignup}
-    //             >
+    //             <Button className="rounded-full p-6" onClick={handleSignup}>
     //               Signup
     //             </Button>
     //           </TabsContent>
@@ -192,14 +95,105 @@ const Auth = () => {
     //       </div>
     //     </div>
     //     <div className="hidden xl:flex justify-center items-center">
-    //       <img
-    //         src={Background}
-    //         alt="background login"
-    //         className="max-h-[80vh] object-contain"
-    //       />
+    //       <img src={Background} alt="background login" className="h-[700px]" />
     //     </div>
     //   </div>
     // </div>
+
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div
+        className="h-[90vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[90vw] lg:w-[70vw] 
+      xl:w-[60vw] rounded-3xl grid xl:grid-cols-2 overflow-hidden"
+      >
+        <div className="flex flex-col gap-4 items-center justify-center p-4 md:p-6 lg:p-8">
+          <div className="flex flex-col items-center">
+            <div className="flex items-center justify-center">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+                Welcome
+              </h1>
+              <img
+                src={Victory}
+                alt="Victory Emoji"
+                className="h-[50px] md:h-[70px] lg:h-[80px] ml-2"
+              />
+            </div>
+            <p className="text-center mt-2 text-sm md:text-base">
+              Fill in the details to get started with the best chat app
+            </p>
+          </div>
+          <Tabs className="w-full mt-4">
+            <TabsList className="bg-transparent rounded-none w-full">
+              <TabsTrigger
+                value="login"
+                className="text-black text-opacity-90 border-b-2 w-1/2 p-2 md:p-3 data-[state=active]:border-b-purple-500 transition-all duration-300"
+              >
+                Login
+              </TabsTrigger>
+              <TabsTrigger
+                value="signup"
+                className="text-black text-opacity-90 border-b-2 w-1/2 p-2 md:p-3 data-[state=active]:border-b-purple-500 transition-all duration-300"
+              >
+                Signup
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent className="flex flex-col gap-4 mt-4" value="login">
+              <Input
+                placeholder="Email"
+                type="email"
+                className="rounded-full p-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="Password"
+                type="password"
+                className="rounded-full p-3"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button className="rounded-full p-3" onClick={handleLogin}>
+                Login
+              </Button>
+            </TabsContent>
+            <TabsContent className="flex flex-col gap-4 mt-4" value="signup">
+              <Input
+                placeholder="Email"
+                type="email"
+                className="rounded-full p-3"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="Password"
+                type="password"
+                className="rounded-full p-3"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Input
+                placeholder="Confirm Password"
+                type="password"
+                className="rounded-full p-3"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <Button className="rounded-full p-3" onClick={handleSignup}>
+                Signup
+              </Button>
+            </TabsContent>
+          </Tabs>
+        </div>
+        <div className="hidden xl:flex justify-center items-center p-4">
+          <div className="max-w-full max-h-full flex justify-center items-center overflow-hidden">
+            <img
+              src={Background}
+              alt="background login"
+              className="object-contain max-h-[400px] max-w-full"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log(err.message));
