e959e7d7fcc5901f32c571ae5135219e79196f90
0x1Shub <shubhamyeram13@gmail.com>
2025-02-02 03:43:13 +0530

    Fixed CORS issue

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -20,8 +20,6 @@ export const signup = async (req, res, next) => {
     const user = await User.create({ email, password });
     res.cookie("jwt", createToken(email, user.id), {
       maxAge,
-      secure: true,
-      sameSite: "None",
     });
     return res.status(201).json({
       user: {
@@ -53,8 +51,6 @@ export const login = async (req, res, next) => {
 
     res.cookie("jwt", createToken(email, user.id), {
       maxAge,
-      secure: true,
-      sameSite: "None",
     });
     return res.status(200).json({
       user: {
@@ -182,7 +178,7 @@ export const removeProfileImage = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
   try {
-    res.cookie("jwt", "", { maxAge: 1, secure: true, sameSite: "None" });
+    res.cookie("jwt", "", { maxAge: 1 });
     return res.status(200).send("Logout successfully.");
   } catch (err) {
     console.log(err);
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN],
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
